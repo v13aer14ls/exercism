3cc9bc79ccfa7ea5a786d7bd2c296fd5f80f3b2c
Kether <v13aer14ls@gmail.com>
2018-07-18 18:22:39 -0300

    mais um script inutil

---
+++ simples.py
@@ -0,0 +1,8 @@
+# Python 3.x
+# Mantainer = Guilherme Amaral
+
+nome = input("Qual é o seu nome ? ")
+if nome == "Guilherme":
+	print ("Mas que nome maravilhoso !")
+else:
+	print ("Olá, " + nome + " !")

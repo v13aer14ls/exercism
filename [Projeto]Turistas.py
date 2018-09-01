50f56c1b1ae0667c28e280e06fa2f1607b9878a9
Kether <v13aer14ls@gmail.com>
2018-09-01 13:12:42 -0300

    adicionando objetos de teste

--- [Projeto]Turistas.py
+++ [Projeto]Turistas.py
@@ -1,2 +1,3 @@
 #!/bin/python2x/env
 destinations = ["Paris, France","Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]

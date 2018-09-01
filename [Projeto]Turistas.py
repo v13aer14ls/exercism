5c22fc4b820d07350d10eb544872be5abf7747ae
Kether <v13aer14ls@gmail.com>
2018-09-01 15:22:46 -0300

    termino

--- [Projeto]Turistas.py
+++ [Projeto]Turistas.py
@@ -4,6 +4,7 @@
 destinations = ["Paris, France","Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
+
 def get_destination_index(destination):
     destination_index = destinations.index(destination)
     return destination_index

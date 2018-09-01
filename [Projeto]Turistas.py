8ea97e40656374095a5a5b0a36f9f8a6200344df
Kether <v13aer14ls@gmail.com>
2018-09-01 14:01:09 -0300

    Adicionando atrações

--- [Projeto]Turistas.py
+++ [Projeto]Turistas.py
@@ -1,5 +1,5 @@
 #!/bin/python2x/env
-destinations = ["Paris, France","Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+destinations = ["Paris, France","Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
 def get_destination_index(destination):
@@ -12,8 +12,27 @@ def get_traveler_location(traveler):
     return traveler_destination_index
 
 test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
-
 attractions = []
+
 for destination in destinations:
-    attractions,append([]),
+    attractions.append([])
+
+def add_attraction(destination,attraction):
+    try:
+        destination_index = get_destination_index(destination)
+        attractions_for_destination  = attractions[destination_index].append(attraction)
+    except SyntaxError:
+        return
+add_attraction("Los Angeles, USA",["Venice Beach",["beach"]])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+print(attractions)
